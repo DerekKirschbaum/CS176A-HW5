4a4afae402dcc14465f26edc9c92d8b6fab9d380
Derek Kirschbaum <dgkirschbaum@ucsb.edu>
2025-12-05 18:52:44 -0800

    updates

--- hangman_client.c
+++ hangman_client.c
@@ -2,10 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdint.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <ctype.h>
+
+static int recv_exact(int fd, void *buf, size_t len) {
+    size_t total = 0;
+    char *p = buf;
+    while (total < len) {
+        ssize_t n = recv(fd, p + total, len - total, 0);
+        if (n <= 0) {
+            return -1;
+        }
+        total += (size_t)n;
+    }
+    return 0;
+}
 
 struct serverMessage {
     short message_flag;
@@ -21,11 +35,23 @@ struct clientMessage {
 
 struct serverMessage server_msg;
 
+// void sendMessage(int client_fd, int length, char data) {
+//     struct clientMessage client_msg;
+//     client_msg.message_length = length;
+//     client_msg.data[0] = data;
+//     send(client_fd, &client_msg, sizeof(client_msg), 0);
+// }
+
 void sendMessage(int client_fd, int length, char data) {
-    struct clientMessage client_msg;
-    client_msg.message_length = length;
-    client_msg.data[0] = data;
-    send(client_fd, &client_msg, sizeof(client_msg), 0);
+    if (length == 0) {
+        uint8_t len = 0;
+        send(client_fd, &len, 1, 0);
+    } else {
+        uint8_t buf[2];
+        buf[0] = 1;
+        buf[1] = (uint8_t)data;
+        send(client_fd, buf, 2, 0);
+    }
 }
 
 char waitForGuess(int client_fd) {
@@ -36,6 +62,7 @@ char waitForGuess(int client_fd) {
         printf(">>>Letter to guess: ");
         if(!fgets(guess, sizeof(guess), stdin)) {
             sendMessage(client_fd, 0, '\0');
+            printf("\n");
             close(client_fd);
             exit(EXIT_SUCCESS);
         }
@@ -52,69 +79,145 @@ char waitForGuess(int client_fd) {
     return g;
 }
 
-void playHangman(int client_fd) {
-    int gameOver = 0;
-    while(!gameOver) {
-        recv(client_fd, &server_msg, sizeof(server_msg), 0);
+// void playHangman(int client_fd) {
+//     int gameOver = 0;
+//     while(!gameOver) {
+//         recv(client_fd, &server_msg, sizeof(server_msg), 0);
 
-        if(server_msg.message_flag != 0) {
-            for(int i = 0; i < server_msg.message_flag; i++) {
-                printf("%c", server_msg.data[i]);
-            }
-        } else {
-            printf(">>>");
-            for (int i = 0; i < server_msg.word_length; i++) {
-                printf("%c", server_msg.data[i]);
-                if (i < server_msg.word_length - 1) {
-                    printf(" ");
-                }
-            }
-            printf("\n");
-            printf(">>>Incorrect Guesses: ");
-            for (int i = 0; i < server_msg.num_incorrect; i++) {
-                printf("%c", server_msg.data[server_msg.word_length + i]);
-                if (i < server_msg.num_incorrect - 1) {
-                    printf(" ");
-                }
-            }
-            printf("\n>>>\n");
-        }
+//         if(server_msg.message_flag != 0) {
+//             for(int i = 0; i < server_msg.message_flag; i++) {
+//                 printf("%c", server_msg.data[i]);
+//             }
+//         } else {
+//             printf(">>>");
+//             for (int i = 0; i < server_msg.word_length; i++) {
+//                 printf("%c", server_msg.data[i]);
+//                 if (i < server_msg.word_length - 1) {
+//                     printf(" ");
+//                 }
+//             }
+//             printf("\n");
+//             printf(">>>Incorrect Guesses: ");
+//             for (int i = 0; i < server_msg.num_incorrect; i++) {
+//                 printf("%c", server_msg.data[server_msg.word_length + i]);
+//                 if (i < server_msg.num_incorrect - 1) {
+//                     printf(" ");
+//                 }
+//             }
+//             printf("\n>>>\n");
+//         }
 
-        if(server_msg.message_flag == 0) {
-            char guess = waitForGuess(client_fd);
-            sendMessage(client_fd, 1, guess);
-        } else {
-            gameOver = 1;
-        }
-    }
+//         if(server_msg.message_flag == 0) {
+//             char guess = waitForGuess(client_fd);
+//             sendMessage(client_fd, 1, guess);
+//         } else {
+//             gameOver = 1;
+//         }
+//     }
     
+// }
+
+void playHangman(int client_fd) {
+    for (;;) {
+        uint8_t flag;
+        if (recv_exact(client_fd, &flag, 1) < 0) {
+            break;
+        }
+
+        if (flag != 0) {
+            uint8_t len = flag;
+            char msg[256];
+
+            if (len > sizeof(msg) - 1) {
+                if (recv_exact(client_fd, msg, sizeof(msg) - 1) < 0) break;
+                msg[sizeof(msg) - 1] = '\0';
+            } else {
+                if (recv_exact(client_fd, msg, len) < 0) break;
+                msg[len] = '\0';
+            }
+
+            printf(">>>%s\n", msg);
+            continue;
+        }
+
+        uint8_t word_len, num_incorrect;
+        if (recv_exact(client_fd, &word_len, 1) < 0) break;
+        if (recv_exact(client_fd, &num_incorrect, 1) < 0) break;
+
+        int total = word_len + num_incorrect;
+        if (total > 255) break;
+
+        char data[256];
+        if (recv_exact(client_fd, data, total) < 0) break;
+
+        printf(">>> ");
+        for (int i = 0; i < word_len; i++) {
+            printf("%c", data[i]);
+            if (i < word_len - 1) {
+                printf(" ");
+            }
+        }
+        printf("\n");
+
+        printf(">>>Incorrect Guesses:");
+        for (int i = 0; i < num_incorrect; i++) {
+            printf(" %c", data[word_len + i]);
+        }
+        printf("\n>>>\n");
+
+        char guess = waitForGuess(client_fd);
+        sendMessage(client_fd, 1, guess);
+    }
 }
+
+
+// void setupHangman(int client_fd) {
+//     if(recv(client_fd, &server_msg, sizeof(server_msg), 0) <= 0) {
+//         exit(EXIT_FAILURE);
+//     }
+
+//     if (server_msg.message_flag != 0 && strcmp(server_msg.data, "server-overloaded") == 0) {
+//         printf(">>>server-overloaded\n");
+//         close(client_fd);
+//         exit(EXIT_SUCCESS);
+//     } else {
+//         printf(">>>Ready to start game? (y/n): ");
+//         char guess[256];
+//         if (!fgets(guess, sizeof(guess), stdin)) {
+//             close(client_fd);
+//             exit(EXIT_SUCCESS);
+//         }
+//         if (tolower(guess[0]) == 'y') {
+//             sendMessage(client_fd, 0, '\0');
+//             playHangman(client_fd);
+//         } else {
+//             close(client_fd);
+//             exit(EXIT_SUCCESS);
+//         }
+//     }
+// }
 
 void setupHangman(int client_fd) {
-    if(recv(client_fd, &server_msg, sizeof(server_msg), 0) <= 0) {
-        exit(EXIT_FAILURE);
-    }
+    char line[256];
 
-    if (server_msg.message_flag != 0 && strcmp(server_msg.data, "server-overloaded") == 0) {
-        printf(">>>server-overloaded\n");
+    printf(">>>Ready to start game? (y/n): ");
+    fflush(stdout);
+
+    if (!fgets(line, sizeof(line), stdin)) {
+        printf("\n");
         close(client_fd);
         exit(EXIT_SUCCESS);
+    }
+
+    if (tolower(line[0]) == 'y') {
+        sendMessage(client_fd, 0, '\0');
+        playHangman(client_fd);
     } else {
-        printf(">>>Ready to start game? (y/n): ");
-        char guess[256];
-        if (!fgets(guess, sizeof(guess), stdin)) {
-            close(client_fd);
-            exit(EXIT_SUCCESS);
-        }
-        if (tolower(guess[0]) == 'y') {
-            sendMessage(client_fd, 0, '\0');
-            playHangman(client_fd);
-        } else {
-            close(client_fd);
-            exit(EXIT_SUCCESS);
-        }
+        close(client_fd);
+        exit(EXIT_SUCCESS);
     }
 }
+
 
 int main (int argc, char* argv[]) {
     if(argc != 3) return -1;
